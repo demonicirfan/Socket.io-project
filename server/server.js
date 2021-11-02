5f005df4235aa60ab4be74397d62811b1dc7b252
Irfan Asif <hashtag.irfan@gmail.com>
2021-11-03 01:01:01 +0530

    nov 3

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');
 const PORT = process.env.PORT || 3000;
 
-var app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Server is up and running on port', PORT);
 });
